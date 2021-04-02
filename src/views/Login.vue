4521535d8ae02be13d98aef28290fd9957b102e0
ww1346 <wmr1346@bupt.edu.cn>
2021-04-02 16:00:35 +0800

    developing_0.0.1

---
+++ src/views/Login.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
+            <h3 class="loginTitle">
+                系统登录
+            </h3>
+            <el-form-item prop="username">
+                <el-input type="text" v-model="loginForm.username" auto-complete="false"
+                          placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input type="password" auto-complete="false" v-model="loginForm.password"
+                          placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <el-form-item prop="code">
+                <el-input type="text" auto-complete="false" v-model="loginForm.code" placeholder="请输入验证码"
+                          style="width: 250px; margin-right: 5px;"></el-input>
+                <img :src="captchaUrl">
+            </el-form-item>
+            <el-checkbox v-model="checked" style="text-align: left; margin: 0px 0px 15px 0px;">记住我</el-checkbox>
+            <el-button type="primary" style="width: 100%" @click="submitLogin">登录</el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data() {
+            return {
+                captchaUrl: '',
+                loginForm: {
+                    username: 'admin',
+                    password: '123',
+                    code: ''
+                },
+                checked: true,
+                rules: {
+                    username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+                    password: [{required: true, message: '请输入密码', trigger: 'blur'}],
+                    code: [{required: true, message: '请输入验证码', trigger: 'blur'}]
+                },
+            }
+        },
+        methods: {
+            submitLogin() {
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        this.$message.error('请输入所有字段！');
+                        return false;
+                    }
+                });
+            },
+        }
+    }
+</script>
+
+<style>
+    .loginContainer {
+        border-radius: 15px;
+        background-clip: padding-box;
+        margin: 180px auto;
+        width: 350px;
+        padding: 15px 35px 15px 35px;
+        background: #ffffff;
+        border: 1px solid #eaeaea;
+        box-shadow: 0 0 25px #cac6c6;
+    }
+
+    .loginTitle {
+        margin: 0px auto 40px auto;
+        text-align: center;
+    }
+</style>
